e309258f7243039c78437e11afe4151dede0cf0d
Marjo Forcado <marjoforcado@gmail.com>
2021-01-14 19:48:58 +0800

    Fix layout

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import {
-  useStaticQuery,
   graphql,
+  useStaticQuery,
 } from 'gatsby';
 
 import {
   Box,
   Container,
   CssBaseline,
+  Grid,
 } from '@material-ui/core';
 
 import {
@@ -17,9 +18,7 @@ import {
   Navbar,
 } from '@shared/components';
 
-import displayImage from '@images/image-1.jpg';
-
-const MainLayout = (props) => {
+const MainLayout = props => {
   const { children } = props;
 
   const sidebarWidth = 250;
@@ -39,28 +38,11 @@ const MainLayout = (props) => {
   return (
     <>
       <CssBaseline />
-      <Box style={{
-          position: 'fixed',
-          height: '100%',
-          // backgroundColor: process.env.APP_COLOR_PRIMARY,
-          width: `${sidebarWidth}px`,
-          top: 0,
-          left: 0,
-        }}>
-        <img src={displayImage} />
-      </Box>
-      <Box style={{
-             width: `calc(100% - ${sidebarWidth}px)`,
-             position: 'absolute',
-             right: 0,
-             height: '100%',
-           }}>
-        <Navbar siteTitle={data.site.siteMetadata?.title || 'Title'} />
-        <Container className="py-3">
-          {children}
-        </Container>
-        <Footer />
-      </Box>
+      <Navbar siteTitle={data.site.siteMetadata?.title || 'Title'} />
+      <Container>
+        {children}
+      </Container>
+      <Footer />
     </>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import {
-  Grid,
-} from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
 
 import MainLayout from '@layouts/MainLayout';
 
@@ -15,14 +13,23 @@ import {
   ToolsList,
 } from '@shared/components';
 
+import displayImage from '@images/image-1.jpg';
+
 const IndexPage = () => (
   <MainLayout>
     <SEO title="Home" />
     <MFTypography variant="h3"
                   align="center">@Mrjfrcdo</MFTypography>
-    <Grid spacing={5}
+    <Grid spacing={3}
           container>
-      <Grid xs={8}
+      <Grid xs={3}
+            item>
+        <Paper variant="outlined"
+               elevation={0}>
+          <img src={displayImage} />
+        </Paper>
+      </Grid>
+      <Grid xs={5}
             item>
         <ExperienceList />
       </Grid>
